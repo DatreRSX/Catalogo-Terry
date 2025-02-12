218c7307a781c767b2813df4f5eefdff49182602
DatreRSX <knowsphone@gmail.com>
2025-02-12 19:18:02 +0100

    hola

---
+++ main.js
@@ -0,0 +1,12 @@
+let tabla = new Array (10);
+
+tabla[0] = ["Shelby GT 500 1967","shelby.jpg"] ;
+tabla[1] = ["BMW E32 740i V8","e32.jpg"] ;
+tabla[2] = ["BMW E46 M3 CLS","e46.jpg"] ;
+tabla[3] = ["Citröen DS 21 (Tiburon)","tiburon.jpg"] ;
+tabla[4] = ["Citröen C5 2005 MK2","c5.jpg"] ;
+tabla[5] = ["Seat Leon MK1","shelby.jpg"] ;
+tabla[6] = ["BMW E36 M3","e36.jpg"] ;
+tabla[7] = ["BMW E60 M5 V10","e60,jpg"] ;
+tabla[8] = ["Toyota Supra MK3","supra.jpg"] ;
+tabla[9] = ["Nissan r34 Skyline","r34.jpg"] ;
